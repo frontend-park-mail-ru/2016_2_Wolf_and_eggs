f4d2262c6b39e74d4121dc7202cb8ef511841024
utrobin <patr.rus@gmail.com>
2016-09-18 23:54:46 +0300

    [dz1] forms sent count + plural

--- public/main.js
+++ public/main.js
@@ -1,29 +1,46 @@
 'use strict';
-require("./css/test.scss");
-//require("./css/main.css");
-/**
-* @see http://artsiom.mezin.eu/technofront/
-*/
-function onSubmit (form) {
+
+function onSubmit(form) {
 	let data = {
 		user: form.elements['user'].value,
-		email: form.elements['email'].value
+		email: form.elements['email'].value,
 	};
 
+
+	// if (result === '100'){
+	//     form.hidden = true;
+	// }
+
 	let result = request('/users', data);
+	let obj = JSON.parse(result);
+	let count = obj.count;
+	let name = obj.name;
 
-	if (result === '100') {
-		form.hidden = true;
-		window.helloWorld.innerHTML = hello(data.user); 
-	}
-
-	console.log(data, result);
+	window.welcome.innerHTML = plural(count);
+	doIt(name);
 }
 
-function hello (text) {
-	return 'Привет, ' + text;
+function plural(count) {
+	if (count == 0) {
+		return "Здравствуй, дух";
+	}
+	if (count == 1) {
+		return "Рады приветствовать на нашем курсе!";
+	}
+	if (count > 1 && count < 15) {
+		return "Кликай дальше!! Еще осталось " + (15 - count) + " раз(а)";
+	}
+
+	else {
+		return "01001000 01101001 00101100 00100000 01100010 01110010 01101111";
+	}
+}
+
+function hello (arg) {
+	return "Привет, " + arg;
 }
 
 if (typeof exports === 'object') {
 	exports.hello = hello;
+	exports.plural = plural;
 }
--- server.js
+++ server.js
@@ -8,10 +8,20 @@ app.use('/', express.static('public'));
 app.use(parser.json());
 app.use(technologger);
 
+var emails = {};
+
 app.post('/users', (req, res) => {
-    console.log(req.body);
-    res.send('100');
-    // TODO: вернуть количество обращений
+    let email = req.body.email;
+    let name = req.body.user;
+
+    if (email in emails) {
+        emails[email].count++;
+        res.send(emails[email]);
+    }
+    else {
+        emails[email] = {name: name, count: 0, email: email};
+        res.send(emails[email]);
+    }
 });
 
 app.listen(process.env.PORT || 3000, () => {
