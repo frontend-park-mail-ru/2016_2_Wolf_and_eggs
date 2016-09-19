34ae0ac71dabc4c845e14d60d17d57b3fd2da7b9
utrobin <patr.rus@gmail.com>
2016-09-19 17:40:39 +0300

    [dz1] plural function, form sent counter

--- public/main.js
+++ public/main.js
@@ -1,29 +1,63 @@
 'use strict';
-require("./css/test.scss");
-//require("./css/main.css");
-/**
-* @see http://artsiom.mezin.eu/technofront/
-*/
-function onSubmit (form) {
-	let data = {
-		user: form.elements['user'].value,
-		email: form.elements['email'].value
-	};
-
-	let result = request('/users', data);
-
-	if (result === '100') {
-		form.hidden = true;
-		window.helloWorld.innerHTML = hello(data.user); 
-	}
-
-	console.log(data, result);
+if (typeof exports === 'object') {
+  var RussianName = require('./name').RussianName;
 }
 
-function hello (text) {
-	return 'Привет, ' + text;
+let inflectionWord = (word, gcase) => {
+  try {
+    let w = new RussianName(word);
+    switch (gcase) {
+      case 'gcaseIm':
+        return w.fullName(w.gcaseIm);
+
+      case 'gcaseRod':
+        return w.fullName(w.gcaseRod);
+
+      case 'gcaseDat':
+        return w.fullName(w.gcaseIm);
+
+      case 'gcaseVin':
+        return w.fullName(w.gcaseVin);
+
+      case 'gcaseTvor':
+        return w.fullName(w.gcaseTvor);
+
+      case 'gcasePred':
+        return w.fullName(w.gcasePred);
+
+      default:
+        return word;
+    }
+  } catch (e) {
+    return word + ' error'
+  }
+};
+
+function onSubmit(form) {
+  let data = {
+    user: form.elements['user'].value,
+    email: form.elements['email'].value,
+  };
+
+  let result = request('/users', data);
+  let obj = JSON.parse(result);
+  let count = obj.count;
+  let name = obj.name;
+
+  window.welcome.innerHTML = plural(count, name);
+}
+
+function plural(count, name) {
+  if ((count % 10 == 2 || count % 10 == 3 || count % 10 == 4) && (count < 12 || count > 14)) {
+    let temp = inflectionWord('раз', 'gcaseRod');
+    return `Здравствуйте ${name}, вы вошли ${count} ${temp}`;
+  }
+  else {
+    let temp = inflectionWord('раз', 'gcaseIm');
+    return `Здравствуйте ${name}, вы вошли ${count} ${temp}`;
+  }
 }
 
 if (typeof exports === 'object') {
-	exports.hello = hello;
+  exports.plural = plural;
 }
