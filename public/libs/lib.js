97a21828d10d1a1ee520c0403c89c18ae76a033b
Schtolc <pavelgolubev345@gmail.com>
2016-09-24 14:19:46 +0300

    [Issue #4] Removed name.js. Reworked lib.js

--- public/libs/lib.js
+++ public/libs/lib.js
@@ -1,9 +1,7 @@
-function request(url, data) {
+export default function request(url, data) {
 	let xhr = new XMLHttpRequest();
-	
 	xhr.open('POST', url, false);
 	xhr.setRequestHeader('Content-Type', 'application/json');
 	xhr.send(JSON.stringify(data));
-	
 	return xhr.responseText;
 }
